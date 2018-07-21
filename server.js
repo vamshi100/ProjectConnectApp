b44823f3cdc9959dd4733708bd20074400b66f6d
Vamshi <vamshianireddy@gmail.com>
2018-07-21 03:36:46 -0400

    package.json updated

--- server.js
+++ server.js
@@ -33,14 +33,14 @@ app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
 
-// server static assets if in prod
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static('client/build'))
+  // Set static folder
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000
